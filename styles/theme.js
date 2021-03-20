3a4c307087bfc8a0d8017002ed16b6b8a4ad56d9
Rick <richard.mcgavin@gmail.com>
2021-03-20 00:16:35 -0400

    initial setup

---
+++ styles/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  colors:{
+    primary: {
+      dark: 'hsl(300, 43%, 22%)',
+      light: 'hsl(333, 80%, 67%)',
+    },
+    neutral: {
+      dark: 'hsl(303, 10%, 53%)',
+      light: 'hsl(300, 24%, 96%)',
+      white: 'hsl(0, 0%, 100%)'
+    }
+  }
+};
